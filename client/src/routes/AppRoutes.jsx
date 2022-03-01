f9451347accf252d27db470273f9ebfe33c6aae4
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-03-01 15:11:57 +0100

    merged

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import IndexPage from "../pages/IndexPage/IndexPage";
-import RegisterPage from "../pages/RegisterPage";
-=======
-import IndexPage from "../pages/IndexPage";
 import RegisterPage from "../pages/registerPage/RegisterPage";
->>>>>>> 9bbbcf611f9fc8b598d0f323c0d0da2d5521cd7b
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -14,14 +9,8 @@ const AppRoutes = () => {
   return (<>
   
     <Routes>
-<<<<<<< HEAD
-   
-      <Route path="/" element={<IndexPage />}/>
-      <Route path="/restaurante/registro" element={<RegisterPage/>}></Route>
-=======
       <Route path="/" element={<IndexPage></IndexPage>}/>
       <Route path="/restaurante/registro" element={<RegisterPage></RegisterPage>}></Route>
->>>>>>> 9bbbcf611f9fc8b598d0f323c0d0da2d5521cd7b
     </Routes>
     </>
   );
